36e3709511d240a0227bf3042abb8a6cb2a2fea3
Theo Paris <theoplaysmc@gmail.com>
2018-07-08 15:55:05 -0700

    Revised some of the code. Added a readme file.

---
+++ readme.md
@@ -0,0 +1,10 @@
+**>>>Creepinson's Spigot Plugin Template for Complete Beginners<<< **
+**Thank you for using my plugin template! It is free to use in any of your plugins, just please credit me. That's all I request.**
+*This template includes an example command, and an example event listener.
+*This template also includes a few utilities made both by me, and some other programmers out there.*
+==============================================================
+
+**If you want to add additional libraries to your plugin, just put them in the libs folder, and make sure to not delete any of the following libraries required! If they do need updating go ahead and replace the files just don't get rid of the required libraries.**
+- **Spigot itself (1.12)**
+
+
---
+++ src/me/creepinson/plugin/util/entity/PathfinderUtil.java
@@ -0,0 +1,31 @@
+package me.creepinson.plugin.util.entity;
+
+import java.lang.reflect.Field;
+
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
+import org.bukkit.entity.Entity;
+
+import com.google.common.collect.Sets;
+
+import net.minecraft.server.v1_12_R1.EntityInsentient;
+import net.minecraft.server.v1_12_R1.PathfinderGoalSelector;
+
+
+public class PathfinderUtil {
+	
+	public void removePathFinders(Entity entity) {
+		net.minecraft.server.v1_12_R1.Entity nmsEntity = ((CraftEntity) entity).getHandle();
+		try {
+			Field bField = PathfinderGoalSelector.class.getDeclaredField("b");
+			bField.setAccessible(true);
+			Field cField = PathfinderGoalSelector.class.getDeclaredField("c");
+			cField.setAccessible(true);
+			bField.set(((EntityInsentient) nmsEntity).goalSelector, Sets.newLinkedHashSet());
+			bField.set(((EntityInsentient) nmsEntity).targetSelector, Sets.newLinkedHashSet());
+			cField.set(((EntityInsentient) nmsEntity).goalSelector, Sets.newLinkedHashSet());
+			cField.set(((EntityInsentient) nmsEntity).targetSelector, Sets.newLinkedHashSet());
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
